f52dab9efb5f77c4bfd526f6aab230ea25968455
融雪时分 <cutedays@icloud.com>
2023-04-29 19:40:21 +0800

    Upload files

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+This repository holds the project website of the paper *neuropara* (not yet made public). 
+
+## Credit
+This page was built upon the [Academic Project Page Template](https://github.com/eliahuhorwitz/Academic-project-page-template) ([CC BY-SA 4.0](http://creativecommons.org/licenses/by-sa/4.0/)).
---
+++ static/results/scenes/bedroom/data.js
@@ -0,0 +1,142 @@
+const data =
+{
+    "imageBoxes": [
+        {
+            "title": "Images",
+            "elements": [
+                {
+                    "title": "Reference",
+                    "version": "-",
+                    "image": "Reference.png"
+                },
+                {
+                    "title": "PT (BSDF)",
+                    "version": "-",
+                    "image": "PT (BSDF).png"
+                },
+                {
+                    "title": "Muller et al.",
+                    "version": "-",
+                    "image": "Muller et al..png"
+                },
+                {
+                    "title": "Rath et al.",
+                    "version": "-",
+                    "image": "Rath et al..png"
+                },
+                {
+                    "title": "Ours (radiance)",
+                    "version": "-",
+                    "image": "Ours (radiance).png"
+                },
+                {
+                    "title": "Ours (product)",
+                    "version": "-",
+                    "image": "Ours (product).png"
+                }
+            ]
+        },
+        {
+            "title": "MAPE",
+            "elements": [
+                {
+                    "title": "PT (BSDF)",
+                    "version": "-",
+                    "image": "PT (BSDF)-MAPE.png"
+                },
+                {
+                    "title": "Muller et al.",
+                    "version": "-",
+                    "image": "Muller et al.-MAPE.png"
+                },
+                {
+                    "title": "Rath et al.",
+                    "version": "-",
+                    "image": "Rath et al.-MAPE.png"
+                },
+                {
+                    "title": "Ours (radiance)",
+                    "version": "-",
+                    "image": "Ours (radiance)-MAPE.png"
+                },
+                {
+                    "title": "Ours (product)",
+                    "version": "-",
+                    "image": "Ours (product)-MAPE.png"
+                }
+            ]
+        },
+        {
+            "title": "MRSE",
+            "elements": [
+                {
+                    "title": "PT (BSDF)",
+                    "version": "-",
+                    "image": "PT (BSDF)-MRSE.png"
+                },
+                {
+                    "title": "Muller et al.",
+                    "version": "-",
+                    "image": "Muller et al.-MRSE.png"
+                },
+                {
+                    "title": "Rath et al.",
+                    "version": "-",
+                    "image": "Rath et al.-MRSE.png"
+                },
+                {
+                    "title": "Ours (radiance)",
+                    "version": "-",
+                    "image": "Ours (radiance)-MRSE.png"
+                },
+                {
+                    "title": "Ours (product)",
+                    "version": "-",
+                    "image": "Ours (product)-MRSE.png"
+                }
+            ]
+        }
+    ],
+    "stats": [
+        {
+            "title": "Stats",
+            "labels": [
+                "PT (BSDF)",
+                "Muller et al.",
+                "Rath et al.",
+                "Ours (radiance)",
+                "Ours (product)"
+            ],
+            "series": [
+                {
+                    "label": "MAPE",
+                    "data": [
+                        "0.107730",
+                        "0.076733",
+                        "0.068910",
+                        "0.066175",
+                        "0.064972"
+                    ],
+                    "track": {
+                        "x": [],
+                        "y": []
+                    }
+                },
+                {
+                    "label": "MRSE",
+                    "data": [
+                        "0.025753",
+                        "0.013859",
+                        "0.011049",
+                        "0.010241",
+                        "0.009912"
+                    ],
+                    "track": {
+                        "x": [],
+                        "y": []
+                    }
+                }
+            ]
+        }
+    ]
+}
