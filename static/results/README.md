79196a95b086e3c113dc2a2536c4162c09b4f845
融雪时分 <cutedays@icloud.com>
2023-05-29 16:11:04 +0800

    upload files

---
+++ static/results/README.md
@@ -0,0 +1,4 @@
+# README
+
+This supplemental material contains an interactive viewer for more detailed results in the experiment within the main paper, using the same experiment setup (as Fig.3 and Fig. 7). Directly open the [index.html](index.html) to check the results. The detailed statistics of these 10 test scenes could be seen within the Tab. 2 of the main paper.
+
---
+++ static/results/scenes/breakfast-room/data.js
@@ -0,0 +1,142 @@
+const data =
+{
+    "imageBoxes": [
+        {
+            "title": "Images",
+            "elements": [
+                {
+                    "title": "Reference",
+                    "version": "-",
+                    "image": "Reference.png"
+                },
+                {
+                    "title": "PT (BSDF)",
+                    "version": "-",
+                    "image": "PT (BSDF).png"
+                },
+                {
+                    "title": "Muller et al.",
+                    "version": "-",
+                    "image": "Muller et al..png"
+                },
+                {
+                    "title": "Rath et al.",
+                    "version": "-",
+                    "image": "Rath et al..png"
+                },
+                {
+                    "title": "Ours (radiance)",
+                    "version": "-",
+                    "image": "Ours (radiance).png"
+                },
+                {
+                    "title": "Ours (product)",
+                    "version": "-",
+                    "image": "Ours (product).png"
+                }
+            ]
+        },
+        {
+            "title": "MAPE",
+            "elements": [
+                {
+                    "title": "PT (BSDF)",
+                    "version": "-",
+                    "image": "PT (BSDF)-MAPE.png"
+                },
+                {
+                    "title": "Muller et al.",
+                    "version": "-",
+                    "image": "Muller et al.-MAPE.png"
+                },
+                {
+                    "title": "Rath et al.",
+                    "version": "-",
+                    "image": "Rath et al.-MAPE.png"
+                },
+                {
+                    "title": "Ours (radiance)",
+                    "version": "-",
+                    "image": "Ours (radiance)-MAPE.png"
+                },
+                {
+                    "title": "Ours (product)",
+                    "version": "-",
+                    "image": "Ours (product)-MAPE.png"
+                }
+            ]
+        },
+        {
+            "title": "MRSE",
+            "elements": [
+                {
+                    "title": "PT (BSDF)",
+                    "version": "-",
+                    "image": "PT (BSDF)-MRSE.png"
+                },
+                {
+                    "title": "Muller et al.",
+                    "version": "-",
+                    "image": "Muller et al.-MRSE.png"
+                },
+                {
+                    "title": "Rath et al.",
+                    "version": "-",
+                    "image": "Rath et al.-MRSE.png"
+                },
+                {
+                    "title": "Ours (radiance)",
+                    "version": "-",
+                    "image": "Ours (radiance)-MRSE.png"
+                },
+                {
+                    "title": "Ours (product)",
+                    "version": "-",
+                    "image": "Ours (product)-MRSE.png"
+                }
+            ]
+        }
+    ],
+    "stats": [
+        {
+            "title": "Stats",
+            "labels": [
+                "PT (BSDF)",
+                "Muller et al.",
+                "Rath et al.",
+                "Ours (radiance)",
+                "Ours (product)"
+            ],
+            "series": [
+                {
+                    "label": "MAPE",
+                    "data": [
+                        "0.054320",
+                        "0.044711",
+                        "0.036940",
+                        "0.033485",
+                        "0.031816"
+                    ],
+                    "track": {
+                        "x": [],
+                        "y": []
+                    }
+                },
+                {
+                    "label": "MRSE",
+                    "data": [
+                        "0.006153",
+                        "0.004329",
+                        "0.002977",
+                        "0.002420",
+                        "0.002217"
+                    ],
+                    "track": {
+                        "x": [],
+                        "y": []
+                    }
+                }
+            ]
+        }
+    ]
+}
